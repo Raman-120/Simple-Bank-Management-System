5fe642967a4333fb53fa1226125570f3f4c1c051
Raman-120 <ramanchaulagain059@gmail.com>
2026-06-06 22:06:18 +0545

    set up the user class

--- BankManagementSystem/src/User.java
+++ BankManagementSystem/src/User.java
@@ -1,2 +1,19 @@
 public class User {
+
+    private String username;
+    private int pin;
+
+    User(String username, int pin){
+        this.username = username;
+        this.pin = pin;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public int getPin(){
+        return this.pin;
+    }
+
 }
