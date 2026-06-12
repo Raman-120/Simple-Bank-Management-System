bce240f2fe8036dce71e0b2318a394c777783a5e
Raman-120 <ramanchaulagain059@gmail.com>
2026-06-12 13:24:55 +0545

    used serialization to store user's username and password/pin

--- BankManagementSystem/src/Account.java
+++ BankManagementSystem/src/Account.java
@@ -1,13 +1,19 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Account implements Serializable {
 
      private final Scanner scanner;
+     ArrayList<User> users = new ArrayList<>();
      private final BankActivities b;
      String username;
      String pin;
      double balance;
+     File file = new File("Info.ser");
 
 
     Account(Scanner scanner){
@@ -16,18 +22,31 @@ public class Account implements Serializable {
     }
 
     public void CreateAccount(){
-        System.out.print("Enter your first name: ");
-        String firstName = scanner.next();
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))){
 
-        System.out.print("Enter your last name: ");
-        String lastName = scanner.next();
+            System.out.print("Enter your first name: ");
+            String firstName = scanner.next();
 
-        System.out.print("Enter a pin: ");
-        pin = scanner.next();
 
-        username = firstName + lastName;
+            System.out.print("Enter your last name: ");
+            String lastName = scanner.next();
 
-        System.out.println("Your username is " + username);
+            System.out.print("Enter a pin: ");
+            pin = scanner.next();
+
+            username = firstName + lastName;
+
+            System.out.println("Your username is " + username);
+
+            User user = new User(username, pin);
+            users.add(user);
+            oos.writeObject(users);
+            System.out.println("Information saved successfully.");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
     public void Login(){
