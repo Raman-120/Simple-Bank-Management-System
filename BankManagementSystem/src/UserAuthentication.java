ab960d3a394c1502315d3aa2f841b39a115ce508
Raman-120 <ramanchaulagain059@gmail.com>
2026-06-12 13:53:10 +0545

    Added UserAuthentication feature

--- BankManagementSystem/src/UserAuthentication.java
+++ BankManagementSystem/src/UserAuthentication.java
@@ -1,17 +1,48 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UserAuthentication {
 
-    String username;
-    String pin;
+    String inputUsername;
+    String inputPin;
     private final Scanner scanner;
+    File file = new File("Info.ser");
+    boolean authenticator;
 
     UserAuthentication(Scanner scanner){
         this.scanner = scanner;
     }
-    public void demo(){
+    public void UserAuthenticate(){
         System.out.println("Enter your name: ");
-        username = scanner.next();
+        inputUsername = scanner.next();
+
+        System.out.println("Enter your pin: ");
+        inputPin = scanner.next();
+
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))){
+            ArrayList<User> users = (ArrayList<User>) ois.readObject();
+            authenticator = false;
+
+            for(User user : users){
+                if(inputUsername.equals(user.getUsername()) && inputPin.equals(user.getPin())){
+                    System.out.println("Successfully login.");
+                    authenticator = true;
+                    break;
+                }
+            }
+
+            if(authenticator){
+                System.out.println("Login successfully!");
+            }
+            else {
+                System.out.println("Unable to login \nPlease Register your account.");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 
