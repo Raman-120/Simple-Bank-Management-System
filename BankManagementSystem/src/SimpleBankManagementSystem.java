f45c37f29c44bf57adf1b584695f4412d79ad0e0
Raman-120 <ramanchaulagain059@gmail.com>
2026-06-30 08:59:13 +0545

    removed previous commented codes

--- BankManagementSystem/src/SimpleBankManagementSystem.java
+++ BankManagementSystem/src/SimpleBankManagementSystem.java
@@ -17,7 +17,6 @@ public class SimpleBankManagementSystem {
             account.Login();
         } else {
             System.out.println("Invalid choice.");
-            scanner.close();
             return;
         }
 
